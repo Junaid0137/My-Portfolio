27d3662f8ac0eae844eb505f307e47a3154e96d2
SYED AMEER JUNAID <junaid0825@gmail.com>
2024-08-27 21:07:37 +0530

    Update About.tsx

--- components/About.tsx
+++ components/About.tsx
@@ -16,7 +16,7 @@ function About() {
                 }}
                 whileInView={{ x: 0, opacity: 1, }}
                 transition={{ duration: 1.2 }}
-                className=' -mb-20 ml-4 flex-shrink-0 h-56 w-56 rounded-full object-cover md:h-95 md:w-64 md:rounded-lg md:mb-0 xl:h-42 xl:w-42' src='https://camo.githubusercontent.com/3fb12eff4f34c4a8a2680dae63b2337a4b6b1e831acaac47d9e21ad655c8480b/68747470733a2f2f692e67697068792e636f6d2f3369794b484d494b6735565747367148556d2e77656270' alt=''
+                className=' -mb-20 ml-4 flex-shrink-0 h-56 w-56 rounded-full object-cover md:h-95 md:w-64 md:rounded-lg md:mb-0 xl:h-42 xl:w-42' src='https://camo.githubusercontent.com/1c0c559fc557049d4cd797f1ca922d2971f2d435c47f31e008376be7066e387c/68747470733a2f2f692e67697068792e636f6d2f3369794b484d494b6735565747367148556d2e77656270' alt=''
             />
             <div className='space-y-10 px-10'>
                 <h4 className='text-4xl font-semibold'>Here a little things about me</h4>
